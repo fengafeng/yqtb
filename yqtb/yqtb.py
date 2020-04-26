9b323aa1d8d51b6d820369bc01741bae9b4175a7
fengafeng <larryxu145@gmail.com>
2020-04-26 21:11:00 +0800

    yqtb

--- yqtb/yqtb.py
+++ yqtb/yqtb.py
@@ -1,7 +1,8 @@
+from bs4 import BeautifulSoup
 import requests
 import time
 import sys
-import regex as re
+#import regex as re
 
 s = requests.Session()
  
@@ -54,6 +55,37 @@ def ranking():
     rk_url = 'https://wx.app.nbpt.edu.cn/yqtb/weixin/yqtb/rank?date=20' + time.strftime('%y-%m-%d')
     rk = s.get(rk_url)
     html = rk.text
+    
+    soup = BeautifulSoup(html,'lxml')
+
+    print('前百名学生排行：')
+    for line in soup.find_all(class_='tab-page')[0].find_all(class_='weui-cell'):
+        result = line.text.split()
+        print('第{}名：{}同学 \t{}  \t{}'.format(result[0],result[1],result[2],result[3]))
+
+    print('\n')
+    print('-'*80)
+            
+    print('\n班级排行及完成率')
+    for line in soup.find_all(class_='tab-page')[1].find_all(class_='weui-cell'):
+        result = line.text.split()
+        print('第{}名\t{}\t\t完成率{}%'.format(result[0],result[1],result[2]))
+
+    print('\n')
+    print('-'*80)
+            
+    print('\n院系完成率')
+    for line in soup.find_all(class_='tab-page')[2].find_all(class_='weui-cell'):
+        result = line.text.split()
+        print('{}   \t完成率{}'.format(result[0],result[1]))
+
+
+"""
+正则写的实在太慢
+def ranking():
+    rk_url = 'https://wx.app.nbpt.edu.cn/yqtb/weixin/yqtb/rank?date=20' + time.strftime('%y-%m-%d')
+    rk = s.get(rk_url)
+    html = rk.text
     print(html)
     reg = re.compile('<div.*?ll__bd.*?5px;\">(.*?)</span>(.*?)<span sty.*?or:#999\">(.*?)</span>.*?</div>', re.S)
     result = re.findall(reg, html)
@@ -61,7 +93,7 @@ def ranking():
     print(result)
     for r in result:
         print("第{}名：\t{}\t{}".format(r[0], r[1].strip(), r[2]))
-
+"""
 
 
 if __name__ == '__main__':
